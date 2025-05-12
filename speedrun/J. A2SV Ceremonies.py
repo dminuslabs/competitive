947c8273ff75def5d0c3cf5aa492558535ceca8a
user <me@mail.co>
2025-05-12 18:14:51 +0000

    add: practice problem

---
+++ speedrun/J. A2SV Ceremonies.py
@@ -0,0 +1,42 @@
+import sys, threading
+import time
+from math import ceil, log2, log10, remainder
+from bisect import bisect_left as bl, bisect_right as br
+from collections import defaultdict, deque, Counter
+from heapq import heappush, heappop, heapify
+from itertools import permutations, combinations
+from typing import * # for leetcode
+
+
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def solve():
+    pass
+
+def main():
+    t = 1
+    # t = int(input())
+    for i in range(t):
+        start_time = time.perf_counter()
+
+        solve()
+        # if solve():
+        #     print("YES")
+        # else:
+        #     print("NO")
+
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        # print(f"Time taken for test case {i+1}: {execution_time:.6f} seconds")
+
+
+if __name__ == "__main__":
+    main()
+
+    # recursive things
+    # sys.setrecursionlimit(1 << 30)
+    # threading.stack_size(1 << 27)
+    # main_thread = threading.Thread(target=main)
+    # main_thread.start()
+    # main_thread.join()
